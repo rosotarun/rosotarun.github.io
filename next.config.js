78f5ee9285a91244e00cc9aa294691338f83cdfc
rosotarun <rosotarun@gmail.com>
2025-11-19 21:48:04 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
+  output: 'export',  // 정적 HTML로 빌드
+  basePath: '',  // 개인 사이트라서 빈 문자열
   images: {
     unoptimized: true,
   },
-};
+}
 
-module.exports = nextConfig;
-
+module.exports = nextConfig
